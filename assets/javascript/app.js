c04bc39c18f14a08a4ba2f20002d66b4f1b2bc52
Bill <wkernan@gmail.com>
2016-05-28 13:52:47 -0500

    changed script link added search button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,9 @@ $('#searchButton').on('click', function() {
 	var startYear = $('#startYear').val();
 	var endYear = $('endYear').val();
 	var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931&q=" + searchTerm + "&page=" + numRecords + "&begin_date=" + startYear + "&end_date=" + endYear;
+	if (numRecords == undefined) numRecords="";
+	if (startYear == undefined) startYear="";
+	if (endYear == undefined) endYear="";
 
 	$.ajax({
 		url: queryUrl,
