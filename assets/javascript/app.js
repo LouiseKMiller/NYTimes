ac1bd294d8fc3a559cd11a1fb245185c54295cc1
Bill <wkernan@gmail.com>
2016-05-28 13:26:02 -0500

    added beginning ajax call, linked js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+$('#searchButton').on('click', function() {
+	var searchTerm = $('#searchTerm').val();
+	var numRecords = $('#numRecords').val();
+	var startYear = $('#startYear').val();
+	var endYear = $('endYear').val();
+	var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931&q=" + searchTerm + "&page=" + numRecords + "&begin_date=" + startYear + "&end_date=" + endYear;
+
+	$.ajax({
+		url: queryUrl,
+		method: 'GET'
+	})
+	.done(function(data) {
+		console.log(data);
+	});
+}
